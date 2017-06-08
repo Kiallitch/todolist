9223b2ffc4cfc8b0bdd3e4d940f419d2413b1615
Kiallitch <davidgunyon84@gmail.com>
2017-06-08 15:07:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// Add your JS here.
+var todoInput = document.querySelector('.new-todo');
+
+var todoList = document.querySelector('.todo-list');
+
+todoInput.addEventListener('keydown', function addListItem(event) {
+
+  if ( event.keyCode == 13 ) {
+
+    event.target.value = "";
+
+    var newTodoLi = document.createElement('li');
+
+    newTodoLi .textContent = todoContent;
+
+    todoList.appendChild(newTodoLi);
+  }
+
+  return event;
+});
